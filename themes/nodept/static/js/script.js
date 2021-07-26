a9a2ece2e44b3e9459eb1e30ac864b3404ea8b6d
ritualdust <lizbeth.porier.m@gmail.com>
2021-07-25 22:48:02 -0400

    add user pref in local storage for image quality

--- themes/nodept/static/js/script.js
+++ themes/nodept/static/js/script.js
@@ -1,26 +1,40 @@
-let imgToggleBtn = document.getElementById("resolution-toggle");
-let imagesToToggle = document.querySelectorAll(".to-toggle");
+const imgToggleBtn = document.getElementById("resolution-toggle");
+const imagesToToggle = document.querySelectorAll(".to-toggle");
+const userPref = localStorage.getItem("image-resolution");
+
+const setHiRes = () => {
+  localStorage.setItem("image-resolution", "high");
+  imgToggleBtn.classList.toggle("h");
+  imgToggleBtn.innerHTML = "Low res";
+  imgToggleBtn.setAttribute("aria-label", "Load low resolution images");
+  imgToggleBtn.title = "Load low resolution images";
+  imagesToToggle.forEach((img) => {
+    img.classList.toggle("low");
+    img.src = img.dataset.srchi;
+  });
+}
+
+const setLowRes = () => {
+  localStorage.setItem("image-resolution", "low");
+  imgToggleBtn.classList.toggle("h");
+  imgToggleBtn.innerHTML = "Hi res";
+  imgToggleBtn.setAttribute("aria-label", "Load high resolution images");
+  imgToggleBtn.title = "Load high resolution images";
+  imagesToToggle.forEach((img) => {
+    img.classList.toggle("low");
+    img.src = img.dataset.srclow;
+  });
+}
+
+if(userPref && userPref == "high") {
+  setHiRes();
+}
 
 const onToggle = (e)=> {
   if(imgToggleBtn.classList.contains("h")) {
-    imgToggleBtn.classList.toggle("h");
-    imgToggleBtn.innerHTML = "Low res";
-    imgToggleBtn.setAttribute("aria-label", "Load low resolution images");
-    imgToggleBtn.title = "Load low resolution images";
-    imagesToToggle.forEach((img) => {
-      img.classList.toggle("low");
-      img.src = img.dataset.srchi;
-    });
+    setHiRes();
   } else {
-    imgToggleBtn.classList.toggle("h");
-    imgToggleBtn.innerHTML = "Hi res";
-    imgToggleBtn.setAttribute("aria-label", "Load high resolution images");
-    imgToggleBtn.title = "Load high resolution images";
-    imagesToToggle.forEach((img) => {
-      img.classList.toggle("low");
-      let currentSrc = img.src;
-      img.src = img.dataset.srclow;
-    });
+    setLowRes();
   }
 }
 
